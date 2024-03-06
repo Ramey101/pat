0bd14eec0ff2e959b7c22a0dd808ab495ef9bc69
Ramey101 <aryav101@gmail.com>
2024-03-06 22:09:43 +0800

    initial setup

--- app/page.js
+++ app/page.js
@@ -1,113 +1,117 @@
+
+import { Box, Typography, Container, Button } from "@mui/material"
+import DrawerAppBar from "../component /nav"
 import Image from 'next/image'
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <>
+      <DrawerAppBar />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <Box>
+          <Typography variant="h1" fontFamily={"fantasy"} color={"white"}>PAT</Typography>
+
+          <Container sx={{ display: 'flex', flexDirection: 'row' }}>
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src="/patweb2.png"
+              width={600}
+              height={600}
+              alt="Picture of the author"
             />
-          </a>
-        </div>
-      </div>
+            <Box>
+              <Typography fontFamily={"cursive"} variant="h4" color={"white"}>Hi , I'm Pat. I began my career as a meme trader to spread the laughter across the universe. </Typography>
+              <Button variant="contained" sx={{ mt: '2rem' }}>Be a PAT</Button>
+            </Box>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          </Container>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        </Box>
+      </main>
+      <main className="flex min-h-screen flex-col items-center justify-between">
+        <Typography variant="h2" fontFamily={"fantasy"} color={"white"} style={{ fontWeight: 'bold' }}>ABOUT</Typography>
+        <Container sx={{ display: 'flex', flexDirection: 'row', mt: '3rem' }}>
+          <Typography variant="h5" fontFamily={"serif"} color={"white"} width={2 / 3}>Patick (PAT) woke up one sunny morning in Bikini Bottom, excited for a day of adventure. However, as he looked around, he realized he had nothing planned and nowhere to go. Feeling a mix of boredom and frustration, Pat decided to wander aimlessly around town, hoping something exciting would happen but nothing special happens. </Typography>
+          <Box>
+            <Image
+              src="/patbeach copy.png"
+              width={400}
+              height={400}
+              alt="Picture of the author"
+            />
+          </Box>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+        </Container>
+        <Container sx={{ display: 'flex', flexDirection: 'row', mt: '3rem' }} >
+          <Box >
+            <Image
+              src="/patgate.png"
+              width={300}
+              height={300}
+              alt="Picture of the author"
+            />
+          </Box>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <Typography variant="h5" fontFamily={"serif"} color={"white"} width={2 / 3} >Just as he was about to give up hope, a blockchain portal shaped like a giant Krabby Patty appeared out of nowhere, engulfing Pat and transporting him to a distant galaxy where memes ruled supreme. </Typography>
+        </Container>
+        <Container sx={{ display: 'flex', flexDirection: 'row', mt: '3rem' }}>
+          <Typography variant="h5" fontFamily={"serif"} color={"white"} width={2 / 3}>Surrounded by DOGEs wearing top hats and rare PEPEs doing the Macarena, Pat found himself in a world of blockchain memes, where every transaction was immortalized in the digital ledger with Spongebob's face. Floating in space, Pat realized that sometimes, the most unexpected adventures come when you least expect them, even in the decentralized world of blockchain memes.</Typography>
+          <Box >
+            <Image
+              src="/memesAll.png"
+              width={300}
+              height={300}
+              alt="Picture of the author"
+            />
+          </Box>
+
+        </Container>
+        <Container sx={{ display: 'flex', flexDirection: 'row', mt: '3rem' }}>
+          <Box>
+            <Image
+              src="/btc3.png"
+              width={300}
+              height={300}
+              alt="Picture of the author"
+            />
+          </Box>
+
+          <Typography variant="h5" fontFamily={"serif"} color={"white"} width={2 / 3}>Pat realized that sometimes, the most unexpected adventures come when you least expect them, even in the decentralized world of blockchain memes. And so, he began his new career as a meme trader, swapping Krabby PEPEs for Dogecoins and spreading laughter across the universe.  He traded doge for pepe, pepes for doge and atlast he accumulated lots for BTC , ETH and SOL with the money he made and became a RICHEST GUY IN TH TOWN   </Typography>
+
+        </Container>
+      </main>
+
+      {/* <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <Box>BUY</Box>
+      </main> */}
+
+      <main className="flex flex-col items-center justify-between p-24">
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <Typography variant="h1" fontFamily={"fantasy"} color={"white"} style={{ fontWeight: 'bold' }}>TOKENOMICS</Typography>
+          <Typography fontFamily={"cursive"} variant="h3" color={"white"}>Token Supply:</Typography>
+          <Typography fontFamily={'sans-serif'} variant="h3" color={"white"}>100000000000</Typography>
+          <Box sx={{ bgcolor: 'red', p: '1rem', borderRadius: '2rem', width: '50rem', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <Typography fontFamily={'serif'} variant="h3" color={"white"}> No Taxes, No Bullshit. It’s that simple.
+              LP tokens are burnt, and contract ownership is renounced.  </Typography>
+
+          </Box>
+        </Box>
+      </main>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <Typography variant="h1" fontFamily={"fantasy"} color={"white"} style={{ fontWeight: 'bold' }}>ROADMAP</Typography>
+          <Box sx={{ bgcolor: 'red', p: '1rem', borderRadius: '2rem', width: '50rem', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <Typography fontFamily={'serif'} variant="h3" color={"white"}>Phase I:  10X </Typography>
+            <Typography fontFamily={'serif'} variant="h3" color={"white"}> Phase II: 100X </Typography>
+            <Typography fontFamily={'serif'} variant="h3" color={"white"}> Phase III: 1000X </Typography>
+            <Typography fontFamily={'serif'} variant="h3" color={"white"}> Phase IV:  10000X </Typography>
+            <Typography fontFamily={'serif'} variant="h3" color={"white"}>Phase V:  To the moon</Typography>
+            <Typography fontFamily={'serif'} variant="h3" color={"white"}>......................................</Typography>
+          </Box>
+        </Box>
+      </main>
+    </>
+
   )
 }
